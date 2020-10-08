040455be2d777498bc85b821f0602e87129ea95b
aaron-hardin <aaronrhardin@gmail.com>
2020-10-07 21:47:24 -0400

    Create cmd tool
    Turned main into a command line tool which can be used to convert/round/format currencies

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/aaron-hardin/gopher-money
 
 go 1.15
+
+require github.com/alecthomas/kong v0.2.11
--- main.go
+++ main.go
@@ -7,38 +7,87 @@ import (
 	"github.com/aaron-hardin/gopher-money/format"
 	"github.com/aaron-hardin/gopher-money/money"
 	"github.com/aaron-hardin/gopher-money/rates"
+	"github.com/alecthomas/kong"
 )
 
-func main() {
-	client := rates.NewApiClient("TODO: put key here")
+type Context struct {
+	Debug bool
+}
+
+type ConvertCmd struct {
+	ApiKey string  `name:"apiKey" help:"API key for Open Exchange Rates." env:"GOPHER_MONEY_API_KEY"`
+	From   string  `arg name:"from" help:"From currency."`
+	To     string  `arg name:"to" help:"To currency."`
+	Value  float64 `arg name:"value" help:"Value to convert."`
+}
+
+func (c *ConvertCmd) Run(ctx *Context) error {
+	client := rates.NewApiClient(c.ApiKey)
 	rates, err := client.GetRates()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	converter := money.NewConverter(rates)
-	rawValue, err := converter.Convert("USD", "JPY", 44.3)
+	rawValue, err := converter.Convert(c.From, c.To, c.Value)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	fmt.Println(rawValue)
 
-	roundedValue, err := format.Round("JPY", rawValue)
+	return nil
+}
+
+type RoundCmd struct {
+	Currency string  `arg name:"currency" help:"Currency code to format as."`
+	Value    float64 `arg name:"value" help:"Value to format."`
+}
+
+func (r *RoundCmd) Run(ctx *Context) error {
+	roundedValue, err := format.Round(r.Currency, r.Value)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	fmt.Println(roundedValue)
 
-	displayValue, err := format.Format("JPY", rawValue)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(displayValue)
+	return nil
+}
 
-	displayValue, err = format.FormatAs("JPY", -rawValue, "%s(%v)")
-	if err != nil {
-		log.Fatal(err)
+type FormatCmd struct {
+	Currency string  `arg name:"currency" help:"Currency code to format as."`
+	Value    float64 `arg name:"value" help:"Value to format."`
+	Format   string  `arg optional name:"format" help:"Format to use %v for value and %s for currency symbol."`
+}
+
+func (f *FormatCmd) Run(ctx *Context) error {
+	if f.Format == "" {
+		displayValue, err := format.Format(f.Currency, f.Value)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(displayValue)
+	} else {
+		displayValue, err := format.FormatAs(f.Currency, f.Value, f.Format)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(displayValue)
 	}
-	fmt.Println(displayValue)
+	return nil
+}
+
+var cli struct {
+	Debug bool `help:"Enable debug mode."`
+
+	Convert ConvertCmd `cmd help:"Convert currency."`
+	Round   RoundCmd   `cmd help:"Round currency to nearest whole amount."`
+	Format  FormatCmd  `cmd help:"Format currency."`
+}
+
+func main() {
+	ctx := kong.Parse(&cli)
+	// Call the Run() method of the selected parsed command.
+	err := ctx.Run(&Context{Debug: cli.Debug})
+	ctx.FatalIfErrorf(err)
 }
